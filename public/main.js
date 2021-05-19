00d409b8a793b8ea8af006d996f4dc8918238b3d
Mary Toth <maryarylynn@gmail.com>
2021-05-19 13:57:33 -0400

    allows clicking on both add buttons

--- public/main.js
+++ public/main.js
@@ -1,7 +1,24 @@
 function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  let buttonCounter = 0
+  buttonCounter++
+
+  let teamOneScore = document.querySelector('h3.team1score')
+  teamOneScore.textContent = buttonCounter
+
+  // click + Button
+
+  function handleClickButton() {
+    console.log('You clicked the add button')
   }
+
+  let addAllButtons = document.querySelectorAll('i.add')
+  // let addButton = document.querySelector('i.add')
+
+  // addAllButtons.addEventListener('click', handleClickButton)
+
+  addAllButtons.forEach(button =>
+    button.addEventListener('click', handleClickButton)
+  )
 }
 
 document.addEventListener('DOMContentLoaded', main)
