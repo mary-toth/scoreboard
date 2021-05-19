3a325a74d77bca73d381c8748952ec66cebaa3bf
Mary Toth <maryarylynn@gmail.com>
2021-05-19 15:23:40 -0400

    changing team names works

--- public/main.js
+++ public/main.js
@@ -1,22 +1,47 @@
 function main() {
-  let buttonCounter = 0
-  buttonCounter++
+  //team 1 input box//
 
-  let teamOneScore = document.querySelector('h3.team1score')
-  teamOneScore.textContent = buttonCounter
+  function updateTeam1Name(event) {
+    const teamName = document.querySelector('.team1 h2')
+    teamName.textContent = event.target.value
+    console.log(event.target.value)
+    console.log(event)
+  }
+
+  document
+    .querySelector('.team1 input')
+    .addEventListener('input', updateTeam1Name)
+
+  //team 2 input box//
+
+  function updateTeam2Name(event2) {
+    const team2Name = document.querySelector('.team2 h2')
+    team2Name.textContent = event2.target.value
+    console.log(event2.target.value)
+    console.log(event2)
+  }
+
+  document
+    .querySelector('.team2 input')
+    .addEventListener('input', updateTeam2Name)
+
+  let buttonCounter = 0
+
+  let teamOneScore = document.querySelector('h3')
 
   // click + Button
 
-  function handleClickButton() {
-    console.log('You clicked the add button')
+  function handleClickButton(event) {
+    const thingClickedOn = event.target
+    teamOneScore.textContent = buttonCounter++
+  }
+  function addAPoint() {
+    buttonCounter++
   }
 
-  let addAllButtons = document.querySelectorAll('i.add')
-  // let addButton = document.querySelector('i.add')
+  let allAddButtons = document.querySelectorAll('.add')
 
-  // addAllButtons.addEventListener('click', handleClickButton)
-
-  addAllButtons.forEach(button =>
+  allAddButtons.forEach(button =>
     button.addEventListener('click', handleClickButton)
   )
 }
